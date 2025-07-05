2c1af884bc569e62a496509b88cc1cf05bece554
ShAhilK2 <shahil.kataria@invennico.com>
2025-07-05 14:41:42 +0530

    feat: setup

---
+++ src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import "./../global.css";
+
+export default function RootLayout() {
+  const [fontloaded, error] = useFonts({
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontloaded) SplashScreen.hideAsync();
+  }, [fontloaded, error]);
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
